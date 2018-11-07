5b8e994b2192907d9e19d8f65edf7969d3681e1e
王小林 <984768569@qq.com>
2018-11-07 11:41:12 +0800

    *

--- AOLib/src/main/java/com/kiven/aolib/MyClass.kt
+++ AOLib/src/main/java/com/kiven/aolib/MyClass.kt
@@ -1,3 +1,9 @@
 package com.kiven.aolib
 
-data class User(var name:String, var age:String)
+data class User(var name: String, var age: String)
+data class Teacher(var name: String) {
+    var user: User? = null
+    fun getStudent(): String {
+        return user?.toString() ?: ""
+    }
+}
